50ced0ea55879a9c3cb3a135db8b1cf39f01ecd8
Katie Moran <katiemoran@Katies-MacBook-Pro.local>
2017-10-24 21:29:03 -0400

    created boxplots comparing sleep and diet types

--- GroupB_module9.R
+++ GroupB_module9.R
@@ -11,3 +11,18 @@ head(msleep)
 str(msleep)
 summary(msleep)
 
+## determine number of diet types in msleep
+unique(msleep$vore)
+length(unique(msleep$vore))
+# there are 4 diet types (carni, omni, herbi, and insecti) as well as NAs
+
+# visually determine whether sleep totals vary by diet type
+msleep2 <- msleep[!is.na(msleep$vore), ] #remove rows with no "vore" data
+unique(msleep2$vore) # no longer NAs in vore column
+boxplot(msleep$sleep_total ~ msleep$vore, ylab = "Sleep Totals (hours)", 
+        xlab = "Diet Type")
+
+# The insect eating mammals appear to sleep more than the other other diet types,
+# although there is overlap between the plots and error bars, indicating that the
+# results are not significant. Carnivores, herbivores, and omnivores sleep about
+# the same amount on average, although there is wide variation between the ranges. 
