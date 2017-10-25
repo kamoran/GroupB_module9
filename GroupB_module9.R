b7339cd1fe6e2bd358b909cd8e7e2ceb51d44a66
Katie Moran <katiemoran@Katies-MacBook-Pro.local>
2017-10-24 21:00:55 -0400

    mammal sleep dataset loaded

---
+++ GroupB_module9.R
@@ -0,0 +1,13 @@
+# NR995 Module 9 problem set
+# Using GitHub, plots, functions
+# Erica Holme, Katie Moran, students
+# Last modified: Oct 23 2017
+
+## load mammal sleep dataset through ggplot 2 package
+library(ggplot2)
+data(msleep)
+?msleep
+head(msleep)
+str(msleep)
+summary(msleep)
+
